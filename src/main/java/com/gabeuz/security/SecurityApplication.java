949df4dc17fab294c8a8162f329abe3e89bcc8fd
Gabriel Romero <101878380+Gabeust@users.noreply.github.com>
2025-04-16 08:06:14 -0300

    Update SecurityApplication.java

--- src/main/java/com/gabeuz/security/SecurityApplication.java
+++ src/main/java/com/gabeuz/security/SecurityApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SecurityApplication {
 
 	public static void main(String[] args) {
-		DotenvLoader.load();
+		//DotenvLoader.load();
 		SpringApplication.run(SecurityApplication.class, args);
 	}
 
